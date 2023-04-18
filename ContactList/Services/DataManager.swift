142f1f223ae6adff4e7abc3313d4b6ef440853c2
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-04-18 23:21:07 +0200

    renaming in models v3

--- ContactList/Services/DataManager.swift
+++ ContactList/Services/DataManager.swift
@@ -11,7 +11,7 @@ final class DataManager {
         "Kenny",
         "Kayl",
         "Stan",
-        "Cheef",
+        "Just",
         "Mr",
         "Co-co",
         "The"
@@ -22,7 +22,7 @@ final class DataManager {
         "Mackormik",
         "Braflovski",
         "Marsh",
-        "Just",
+        "Cheef",
         "Mackki",
         "Jambo",
         "Towel"
